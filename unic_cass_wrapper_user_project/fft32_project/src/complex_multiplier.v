527504594c2471c8cd6c4befd4a11913256e24f9
adanlema <alema.9902@gmail.com>
2026-02-25 12:22:06 +0000

    Update complex_multiplier implementation

--- unic_cass_wrapper_user_project/fft32_project/src/complex_multiplier.v
+++ unic_cass_wrapper_user_project/fft32_project/src/complex_multiplier.v
@@ -1,52 +1,65 @@
 module complex_multiplier #(
-    parameter NB_INPUT   = 10,
-    parameter NBF_INPUT  = 7,
+    parameter NB_INPUT_A   = 10,
+    parameter NBF_INPUT_A  = 7,
+    parameter NB_INPUT_B   = 10,
+    parameter NBF_INPUT_B  = 7,
     parameter NB_OUTPUT  = 10,
-    parameter NBF_OUTPUT = 7
+    parameter NBF_OUTPUT = 7,
+    parameter RND_MD     = 0
 )(
     `ifdef USE_POWER_PINS
     inout               VPWR,  // Common digital supply
     inout               VGND,  // Common digital ground
     `endif
-    output reg signed [NB_OUTPUT-1:0] o_real,
-    output reg signed [NB_OUTPUT-1:0] o_imag,
-    input  signed [NB_INPUT-1:0]      i_real_A,
-    input  signed [NB_INPUT-1:0]      i_imag_A,
-    input  signed [NB_INPUT-1:0]      i_real_B,
-    input  signed [NB_INPUT-1:0]      i_imag_B,
-    input                             i_clk
+    input                          i_clk,
+    ///////////////////// INPUTS  /////////////////////
+    input  signed [NB_INPUT_A-1:0] i_real_A,
+    input  signed [NB_INPUT_A-1:0] i_imag_A,
+    input  signed [NB_INPUT_B-1:0] i_real_B,
+    input  signed [NB_INPUT_B-1:0] i_imag_B,
+    ///////////////////// OUTPUTS /////////////////////
+    output signed [ NB_OUTPUT-1:0] o_real,
+    output signed [ NB_OUTPUT-1:0] o_imag
 );
 
-localparam NB_PROD      = NB_INPUT * 2;
-localparam NBF_PROD     = NBF_INPUT * 2;
+///////////////////////////////////////////////////////////////////////////////
+// WIRE AND REGISTER
+///////////////////////////////////////////////////////////////////////////////
+
+localparam NB_PROD      = NB_INPUT_A + NB_INPUT_B;
+localparam NBF_PROD     = NBF_INPUT_A + NBF_INPUT_B;
 localparam NB_PROD_SUM  = NB_PROD + 1;
 localparam NBF_PROD_SUM = NBF_PROD;
 
-wire signed [NB_PROD-1:0] prod_xu; // Real A * Real B
-wire signed [NB_PROD-1:0] prod_yv; // Imag A * Imag B
-wire signed [NB_PROD-1:0] prod_xv; // Real A * Imag B
-wire signed [NB_PROD-1:0] prod_yu; // Imag A * Real B
+wire signed [    NB_PROD-1:0] prod_xu;
+wire signed [    NB_PROD-1:0] prod_yv;
+wire signed [    NB_PROD-1:0] prod_xv;
+wire signed [    NB_PROD-1:0] prod_yu;
+wire signed [NB_PROD_SUM-1:0] sum_re;
+wire signed [NB_PROD_SUM-1:0] sum_im;
+wire signed [  NB_OUTPUT-1:0] w_out_re;
+wire signed [  NB_OUTPUT-1:0] w_out_im;
+reg  signed [  NB_OUTPUT-1:0] r_out_re;
+reg  signed [  NB_OUTPUT-1:0] r_out_im;
+
+///////////////////////////////////////////////////////////////////////////////
+// CL
+///////////////////////////////////////////////////////////////////////////////
 
 assign prod_xu = i_real_A * i_real_B;
 assign prod_yv = i_imag_A * i_imag_B;
 assign prod_xv = i_real_A * i_imag_B;
 assign prod_yu = i_imag_A * i_real_B;
 
-wire signed [NB_PROD_SUM-1:0] sum_re;
-wire signed [NB_PROD_SUM-1:0] sum_im;
-
 assign sum_re = prod_xu - prod_yv;
 assign sum_im = prod_xv + prod_yu;
-
-wire signed [NB_OUTPUT-1:0] w_out_re;
-wire signed [NB_OUTPUT-1:0] w_out_im;
 
 clip_round #(
     .NB_INP(NB_PROD_SUM), 
     .NBF_INP(NBF_PROD_SUM), 
     .NB_OUT(NB_OUTPUT), 
     .NBF_OUT(NBF_OUTPUT),
-    .RND_MD(0)
+    .RND_MD(RND_MD)
 ) u_clip (
     `ifdef USE_POWER_PINS
     .VPWR       (VPWR),
@@ -59,8 +72,11 @@ clip_round #(
 );
 
 always @(posedge i_clk) begin
-    o_real <= w_out_re;
-    o_imag <= w_out_im;
+    r_out_re <= w_out_re;
+    r_out_im <= w_out_im;
 end
-    
+
+assign o_real = r_out_re;
+assign o_imag = r_out_im;
+
 endmodule
